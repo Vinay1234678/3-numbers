1d1518ae0a0e3fd84de2306c323a6bf4707db8ee
Vinay1234678 <93247207+Vinay1234678@users.noreply.github.com>
2021-11-09 11:49:09 +0530

    Add files via upload

---
+++ vinay,c 3 numbers.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+ void main()
+ { int a,b;
+  printf("Enter 1st number");
+  scanf("%d", &a);
+  printf("Enter 2nd number");
+  scanf("%d", &b);
+  printf("number1:%d\n",a);
+  printf("number2:%d\n",b);
+ }
